44e8f20167f82294a3e172629491fdc29d610efd
a <a@as-MacBook-Pro.local>
2021-03-01 21:01:00 -0500

    Load sync on play button press (LoadButtonAsync is likely not used right here -> likely requires coroutine)

--- Assets/Scripts/MainMenuManager/PlayClick.cs
+++ Assets/Scripts/MainMenuManager/PlayClick.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayClick : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class PlayClick : MonoBehaviour
 		Debug.Log("Debug: Play"); 
 		Color orangeFromMuzzle = new Color32(253,175,71,255);
 		text.color = orangeFromMuzzle;
+		SceneManager.LoadSceneAsync("Scene 1");
 	}
     // Start is called before the first frame update
     void Start()
