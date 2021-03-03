cbeffae0e8f1ee93fae38a95990cda604266e8b1
a <a@as-MacBook-Pro.local>
2021-03-02 21:07:26 -0500

    Show muzzleFlash on play click

--- Assets/Scripts/MainMenuManager/PlayClick.cs
+++ Assets/Scripts/MainMenuManager/PlayClick.cs
@@ -14,12 +14,12 @@ public class PlayClick : MonoBehaviour
 		Color orangeFromMuzzle = new Color32(253,175,71,255);
 		text.color = orangeFromMuzzle;
 		SceneManager.LoadSceneAsync("Scene 1");
-		muzzleFlash.setActive(true);
+		muzzleFlash.enabled = true;
 	}
     // Start is called before the first frame update
     void Start()
     {
-        
+       muzzleFlash.enabled = false; 
     }
 
     // Update is called once per frame
