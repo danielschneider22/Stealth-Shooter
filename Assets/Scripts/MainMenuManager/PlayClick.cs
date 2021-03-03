b47001eedf093f0065e6cddedcd3bb5434fe3c82
a <a@as-MacBook-Pro.local>
2021-03-02 20:49:27 -0500

    Placeholder commit: Add muzzleFlash image - add compiler error

--- Assets/Scripts/MainMenuManager/PlayClick.cs
+++ Assets/Scripts/MainMenuManager/PlayClick.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class PlayClick : MonoBehaviour
 {
 	public TextMeshProUGUI text;
+	public Image muzzleFlash;
 
 	public void Play() { // exit main menu into game scene
 		Color orangeFromMuzzle = new Color32(253,175,71,255);
 		text.color = orangeFromMuzzle;
 		SceneManager.LoadSceneAsync("Scene 1");
+		muzzleFlash.setActive(true);
 	}
     // Start is called before the first frame update
     void Start()
