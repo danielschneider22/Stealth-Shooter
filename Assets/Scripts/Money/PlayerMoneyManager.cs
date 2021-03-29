79f88ea8d18af77989d9bd9c7e1067b4e63d3839
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-03-29 17:03:09 -0400

    coin sound effect and bugs

--- Assets/Scripts/Money/PlayerMoneyManager.cs
+++ Assets/Scripts/Money/PlayerMoneyManager.cs
@@ -8,6 +8,12 @@ public class PlayerMoneyManager : MonoBehaviour
     public int money;
     public TextMeshProUGUI moneyText;
     public GameObject moneyChangeText;
+    private AudioManager audioManager;
+
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
 
     public void changeMoney(int changeAmount)
     {
@@ -15,5 +21,6 @@ public class PlayerMoneyManager : MonoBehaviour
         moneyText.text = money.ToString();
         moneyChangeText.GetComponent<TextMeshProUGUI>().text = "+" + changeAmount.ToString();
         moneyChangeText.SetActive(true);
+        audioManager.Play("CoinCollect", 0);
     }
 }
